17cc12b2ac23b91931288982282a232286fe992d
Aylex Nullzero <p4rlx-news@pm.me>
2023-12-21 01:02:23 +1030

    Graph mayhem

---
+++ src/CONSTANTS.ts
@@ -0,0 +1,8 @@
+
+let date = new Date();
+
+CONSTANTS = {
+    filepath_data = ``../utils/output/${date}-${projectName}-data.json`,
+    filepath_graph = ``../utils/output/${date}-${projectName}-graph-final.json`,
+
+}
